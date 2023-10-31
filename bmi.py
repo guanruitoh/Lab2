f53da50eb1e512effa4ab7280296beac6bad73a0
guanrui <guanrui0701@gmail.com>
2023-10-31 14:13:19 +0800

    week3 exercise1 step b

--- bmi.py
+++ bmi.py
@@ -7,11 +7,13 @@ def calculate_bmi(height, weight):
     overweight = 25.0
     if bmi<underweight:
         print("You are Under Weight")
+        return -1
     elif bmi>overweight:
         print("You are Over Weight")
+        return 0
     else:
         print("You are Normal Weight")
-
+        return 1
 
 calculate_bmi(weight=57, height=1.73)
 
